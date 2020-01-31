56efda1db85b2bfd7726a5949676eb29cc576f7d
dzordanq <random05510@gmail.com>
2020-01-31 18:46:51 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,21 +11,18 @@ script_path = os.path.dirname(os.path.abspath(__file__))
 def get_linear_model(model_name):
     with open(os.path.join(script_path, 'Regresja pliki', 'Regresja liniowa', model_name), 'rb') as f:
         regressor = pickle.loads(f.read())
-    # regressor = pickle.load(open(os.path.join(script_path,'Regresja pliki','Regresja liniowa', model_name), 'rb'))
     return regressor
 
 
 def get_polynomial_model(model_name):
     with open(os.path.join(script_path, 'Regresja pliki', 'Regresja wielomianowa', model_name), 'rb') as f:
         regressor = pickle.loads(f.read())
-    #regressor = pickle.load(open(os.path.join(script_path,'Regresja pliki','Regresja wielomianowa', model_name), 'rb'))
     return regressor
 
 
 def get_polynomial_features(model_name):
     with open(os.path.join(script_path, 'Regresja pliki', 'Regresja wielomianowa cechy', model_name), 'rb') as f:
         features = pickle.loads(f.read())
-    #features = pickle.load(open(os.path.join(script_path,'Regresja pliki','Regresja wielomianowa cechy', model_name), 'rb'))
     return features
 
 
